cf966fcb03741c0bbe5a502bb2d24538c2a053af
Tilman Holube <tilman@holube.de>
2025-07-14 17:09:32 +0200

    Parallelization

--- generators/src/main/java/de/holube/ether/generators/image/noise/NoiseImageGenerator.java
+++ generators/src/main/java/de/holube/ether/generators/image/noise/NoiseImageGenerator.java
@@ -6,6 +6,7 @@ import de.holube.ether.generators.image.ImageGeneratorResult;
 import lombok.RequiredArgsConstructor;
 
 import java.awt.image.BufferedImage;
+import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
 public class NoiseImageGenerator implements ImageGenerator {
@@ -19,11 +20,12 @@ public class NoiseImageGenerator implements ImageGenerator {
     @Override
     public ImageGeneratorResult generate() {
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                shader(x, y, image);
-            }
-        }
+
+        IntStream.range(0, width)
+                .parallel()
+                .forEach(x -> IntStream.range(0, height)
+                        .forEach(y -> shader(x, y, image))
+                );
 
         return new ImageGeneratorResult(image);
     }
