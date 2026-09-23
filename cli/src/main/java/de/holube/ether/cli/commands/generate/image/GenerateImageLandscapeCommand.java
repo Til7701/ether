84945e6988c17defd8451af1f55899d8b18005c8
Tilman Holube <tilman@holube.de>
2026-09-23 13:41:53 +0200

    WIP

---
+++ cli/src/main/java/de/holube/ether/cli/commands/generate/image/GenerateImageLandscapeCommand.java
@@ -0,0 +1,66 @@
+package de.holube.ether.cli.commands.generate.image;
+
+import de.holube.ether.cli.color.ColorFactoryFactory;
+import de.holube.ether.cli.mixins.HelpMixin;
+import de.holube.ether.cli.mixins.RangeColorMixin;
+import de.holube.ether.generators.image.ImageGeneratorResult;
+import de.holube.ether.generators.image.noise.NoiseImageGenerator;
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadLocalRandom;
+
+@CommandLine.Command(
+        name = "noise",
+        description = "Generate an image containing noise.",
+        sortOptions = false
+)
+public final class GenerateImageNoiseCommand implements Callable<Integer> {
+
+    @SuppressWarnings("unused")
+    @CommandLine.Mixin
+    private HelpMixin helpMixin;
+
+    @SuppressWarnings("unused")
+    @CommandLine.Mixin
+    private RangeColorMixin rangeColorMixin;
+
+    @SuppressWarnings("unused")
+    @CommandLine.ParentCommand
+    private GenerateImageCommand parentCommand;
+
+    @SuppressWarnings("FieldMayBeFinal")
+    @CommandLine.Option(
+            names = {"--seed"},
+            description = "Seed for the noise generator."
+    )
+    private long seed = ThreadLocalRandom.current().nextLong();
+
+    @SuppressWarnings({"FieldMayBeFinal", "FieldCanBeLocal"})
+    @CommandLine.Option(
+            names = {"--scale"},
+            description = "Scale for the noise generator.",
+            showDefaultValue = CommandLine.Help.Visibility.ALWAYS
+    )
+    private double scale = 0.01;
+
+    @Override
+    public Integer call() {
+        NoiseImageGenerator generator = new NoiseImageGenerator(
+                parentCommand.width(),
+                parentCommand.height(),
+                seed,
+                scale,
+                ColorFactoryFactory.create(rangeColorMixin)
+        );
+
+        ImageGeneratorResult imageResult = generator.generate();
+        return GenerateImageCommand.handleImageResult(
+                imageResult,
+                parentCommand.outputFile(),
+                parentCommand.parentCommand().noGUI(),
+                "Noise"
+        );
+    }
+
+}
---
+++ cli/src/main/java/de/holube/ether/generators/image/landscape/LandscapeImageGenerator.java
@@ -0,0 +1,44 @@
+package de.holube.ether.generators.image.noise;
+
+import de.holube.ether.commons.noise.OpenSimplex2S;
+import de.holube.ether.generators.color.RangeColorFactory;
+import de.holube.ether.generators.image.ImageGenerator;
+import de.holube.ether.generators.image.ImageGeneratorResult;
+import lombok.RequiredArgsConstructor;
+
+import java.awt.image.BufferedImage;
+import java.util.stream.IntStream;
+
+@RequiredArgsConstructor
+public class NoiseImageGenerator implements ImageGenerator {
+
+    private final int width;
+    private final int height;
+    private final long seed;
+    private final double scale;
+    private final RangeColorFactory colorFactory;
+
+    @Override
+    public ImageGeneratorResult generate() {
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        IntStream.range(0, width)
+                .parallel()
+                .forEach(x -> IntStream.range(0, height)
+                        .forEach(y -> shader(x, y, image))
+                );
+
+        return new ImageGeneratorResult(image);
+    }
+
+    private void shader(int x, int y, BufferedImage image) {
+        double n = OpenSimplex2S.noise2(
+                seed,
+                x * scale,
+                y * scale
+        );
+        int color = colorFactory.create(n);
+        image.setRGB(x, y, color);
+    }
+
+}
